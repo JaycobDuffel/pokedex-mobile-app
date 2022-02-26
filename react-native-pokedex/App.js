c77d0049b356e61e0b9f20cf06a86ef44e684f3a
tivings <ivingstaylor@gmail.com>
2022-02-26 00:29:08 -0800

    set up app template for easy testing

--- react-native-pokedex/App.js
+++ react-native-pokedex/App.js
@@ -1,20 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Card from './src/components/card';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.center}>
+      <Card/>
       <StatusBar style="auto" />
     </View>
+
   );
-}
+  }
 
 const styles = StyleSheet.create({
-  container: {
+  center: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#fff'
   },
 });
