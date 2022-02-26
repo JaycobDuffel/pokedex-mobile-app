08b0fab787a1e427d045f7cad45a4d5aa6a7cf85
tivings <ivingstaylor@gmail.com>
2022-02-26 00:29:34 -0800

    initial commit for card component

--- react-native-pokedex/src/components/card.jsx
+++ react-native-pokedex/src/components/card.jsx
@@ -5,9 +5,13 @@ import { View, Text, StyleSheet, Image } from "react-native";
 const Card = () => {
 
   return (
-    <View style={styles.card, styles.grass }>
-      <Image source={require('../../assets/breloom.png')}/>
-      <Text style={styles.text}>Or does it expect something here.</Text>
+    <View style={styles.card, styles.round}>
+      <View style={styles.container}>
+        <Image style={styles.sprite} source={require('../../assets/breloom.png')}/>
+        <Text style={styles.text}>Breloom</Text>
+        <Text style={styles.text}>#238</Text>
+        <Text style={styles.text}>Grass / Fighting</Text>
+      </View>
     </View>
   );
 };
@@ -17,15 +21,37 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    height: 240,
+  },
+
+  round: {
+    borderWidth: 1,
+    borderColor: '#A0D7A0',
+    borderRadius: 8,
+    backgroundColor: '#228B22',
   },
 
   text: {
-    color: '#000'
+    color: '#fff'
+  },
+  
+  container: {
+    paddingHorizontal: 32,
+    paddingVertical: 48
   },
 
   grass: {
     backgroundColor: '#228B22',
   },
+
+  sprite: {
+    position: "absolute",
+    flex:0,
+    alignContent: 'center',
+    alignItems: 'center',
+    top: -50,
+    shadowRadius:25
+  }
 
 });
 
