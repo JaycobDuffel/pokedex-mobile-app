d45243be985b23e5426dd38eb864c8456429445a
Jaycob <jaycobduffel@gmail.com>
2022-02-26 18:31:27 -0800

    Add view for get poke by id

--- pokedex-api/api/api/views/pokemon.py
+++ pokedex-api/api/api/views/pokemon.py
@@ -1,9 +1,20 @@
-from api.models.pokemon import Pokemon
-from rest_framework import viewsets, permissions, generics
+from http.client import HTTPResponse
+from rest_framework import permissions, generics
+
 from api.serializers.pokemon import PokemonSerializer
+from api.models.pokemon import Pokemon
 
 
 class PokemonListCreateView(generics.ListCreateAPIView):
     queryset = Pokemon.objects.all()
     serializer_class = PokemonSerializer
     permission_classes = [permissions.IsAdminUser]
+
+class PokemonDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Pokemon.objects.all()
+    serializer_class = PokemonSerializer
+    permission_classes = [permissions.IsAdminUser]
+
+    def get_object(self):
+        pokemon = Pokemon.objects.filter(id=self.kwargs['pk']).first()
+        return pokemon
