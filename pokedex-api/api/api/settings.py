11d03c2c3592c2e17d183b10b5ded3362c06427d
Jaycob <jaycobduffel@gmail.com>
2022-02-26 16:57:29 -0800

    Remove pagination restframework

--- pokedex-api/api/api/settings.py
+++ pokedex-api/api/api/settings.py
@@ -72,12 +72,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'api.wsgi.application'
 
 
-REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
-}
-
-
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
