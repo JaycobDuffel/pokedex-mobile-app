7d726e77aadc8e860e122bd65c0d3e035a1236a7
Jaycob <jaycobduffel@gmail.com>
2022-02-26 18:31:48 -0800

    Fix pokemon model sprite field types

--- pokedex-api/api/api/models/pokemon.py
+++ pokedex-api/api/api/models/pokemon.py
@@ -5,8 +5,8 @@ class Pokemon(models.Model):
     name = models.CharField(max_length=45)
     pokedex_entry = models.IntegerField()
     type = models.CharField(max_length=15)
-    default_sprite = models.ImageField(upload_to='pokemon/default_sprites/')
-    shiny_sprite = models.ImageField(upload_to='pokemon/shiny_sprites/')
+    default_sprite = models.CharField(max_length=150)
+    shiny_sprite = models.CharField(max_length=150)
     created_datetime = models.DateTimeField(auto_now_add=True)
     updated_datetime = models.DateTimeField(auto_now=True)
     
