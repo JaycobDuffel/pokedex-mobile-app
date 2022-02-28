e09d3e2e403e0e6949350224beda95b27f4bc4ba
tivings <ivingstaylor@gmail.com>
2022-02-27 20:46:35 -0800

    set up working render

--- react-native-pokedex/App.js
+++ react-native-pokedex/App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Card from './src/components/card';
+import breloom from './assets/breloom.png'
 
 export default function App() {
+
   return (
     <View style={styles.center}>
-      <Card/>
+      <Card pokemon="Breloom" type="grass" sprite={breloom}/>
       <StatusBar style="auto" />
     </View>
 
