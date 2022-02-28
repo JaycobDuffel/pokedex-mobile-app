a9db3a10b432699948c9301d00e315732a2d93b3
Jaycob <jaycobduffel@gmail.com>
2022-02-27 22:03:59 -0800

    Create state from context and wrap app in provider

--- react-native-pokedex/App.js
+++ react-native-pokedex/App.js
@@ -1,20 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { SearchContext } from './src/components/Context';
 import Card from './src/components/card';
 import SearchBar from './src/components/SearchBar';
 
 export default function App() {
 
+  const [search, setSearch] = useState('')
   return (
-    <View style={styles.center}>
-      <SearchBar />
-      <Card pokemon="Breloom" type="grass" sprite={''}/>
-      <StatusBar style="auto" />
-    </View>
-
+    <SearchContext.Provider value={{search, setSearch}}>
+      <View style={styles.center}>
+        <SearchBar />
+        <Card pokemon="Breloom" type="grass" sprite={'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/286.png'} />
+        <StatusBar style="auto" />
+      </View>
+    </SearchContext.Provider>
   );
-  }
+}
 
 const styles = StyleSheet.create({
   center: {
